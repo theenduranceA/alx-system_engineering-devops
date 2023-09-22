cad89a7ff2ac4bfa8e4f286b75e8c6bc18cd4ddd
theenduranceA <theenduranceaneke@gmail.com>
2023-09-22 05:32:53 -0700

    All tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file in /tmp
+
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+0x0A. Configuration management - DevOps, SysAdmin, Scripting, CI/CD.
