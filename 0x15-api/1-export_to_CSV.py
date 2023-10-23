125f12d0d143b6acbdf909efa483d42ff7265e33
theenduranceA <theenduranceaneke@gmail.com>
2023-10-23 06:32:24 -0700

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,22 +18,17 @@ def get_employee_data(employee_id):
 
     TASKS_ENDPOINT = BASE_URL + 'todos?userId={}'.format(emp_data.get("id"))
     tasks_data = requests.get(TASKS_ENDPOINT).json()
-
-    total_tasks = len(tasks_data)
-    completed_tasks = [task for task in tasks_data if task["completed"]]
-
-    print("Employee {} has completed {}/{} tasks:"
-          .format(emp_data["name"], len(completed_tasks), total_tasks))
-
-    for task in completed_tasks:
-        print('\t {}'.format(task["title"]))
+    data = {"employee": emp_data, "tasks": tasks_data}
 
     csv_filename = '{}.csv'.format(employee_id)
     with open(csv_filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        username = data['employee']['username']
+        user_id = data['employee']['id']
+
         csv_writer.writerow([
             "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in completed_tasks:
+        for task in data['tasks']:
             csv_writer.writerow([emp_data["id"], emp_data["name"], task[
                 "completed"], task["title"]])
 
